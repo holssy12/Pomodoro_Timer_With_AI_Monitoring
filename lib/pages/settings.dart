8493814ecacef4267abc9006b7583e9c5712b00b
Leejeuk213 <dlwpdnr213@naver.com>
2023-10-11 23:19:01 +0900

    retry2

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -20,12 +20,10 @@ class _SettingsState extends State<Settings> {
   List<String> _sesh = [];
   int _cardNum = 0;
 
-
   @override
   void initState() {
     super.initState();
     _getPrefs();
-
   }
 
   Future<void> _resetTime() async {
@@ -39,20 +37,15 @@ class _SettingsState extends State<Settings> {
       _cardNum = ' / '.allMatches(_data).length;
       final split = _data.split('/');
 
-      _sessions = {
-        for (int i = 0; i < split.length; i++)
-          i: split[i]
-      };
-      for(int i = 1; i < _sessions.length; i++){
+      _sessions = {for (int i = 0; i < split.length; i++) i: split[i]};
+      for (int i = 1; i < _sessions.length; i++) {
         _dates.add(_sessions[i]!.split(' ')[2]);
       }
-      for(int i = 1; i < _sessions.length; i++){
+      for (int i = 1; i < _sessions.length; i++) {
         _sesh.add(_sessions[i]!.split(' ')[1]);
       }
     });
-
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -66,41 +59,68 @@ class _SettingsState extends State<Settings> {
               centerTitle: false,
               backgroundColor: Colors.black,
               title: const Text.rich(
-                  TextSpan(
-                    text: 'History', // text for title
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.greenAccent,
-                      fontFamily: 'Arial',
-                    ),
+                TextSpan(
+                  text: '집중한 기록들', // text for title
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.greenAccent,
+                    fontFamily: 'Arial',
                   ),
                 ),
-
-
+              ),
               actions: [
                 IconButton(
                   padding: const EdgeInsets.only(right: 10.0),
-                  icon: const Icon(Icons.delete_outline, color: Colors.greenAccent, size: 30),
+                  icon: const Icon(Icons.delete_outline,
+                      color: Colors.greenAccent, size: 30),
                   onPressed: () async {
                     bool? delete = await showDialog<bool>(
                       context: context,
                       builder: (context) {
                         return AlertDialog(
                           backgroundColor: Colors.black,
-                          titleTextStyle: const TextStyle(color: Colors.greenAccent, fontSize: 24),
+                          titleTextStyle: const TextStyle(
+                              color: Colors.greenAccent, fontSize: 24),
                           shape: RoundedRectangleBorder(
-                            side: const BorderSide(color: Colors.greenAccent, width: 2),
+                            side: const BorderSide(
+                                color: Colors.greenAccent, width: 2),
                             borderRadius: BorderRadius.circular(15.0),
                           ),
-                          title: const Text('Delete History', style: TextStyle(color: Colors.greenAccent, fontSize: 20, fontFamily: 'Arial',),),
-                          content: const Text('Are you sure you want to delete your ENTIRE session history?', style: TextStyle(color: Colors.greenAccent, fontSize: 16, fontFamily: 'Arial',)),
+                          title: const Text(
+                            '지우시겠습니까?',
+                            style: TextStyle(
+                              color: Colors.greenAccent,
+                              fontSize: 20,
+                              fontFamily: 'Arial',
+                            ),
+                          ),
+                          content: const Text('정말 기록을 지우시겠습니까?',
+                              style: TextStyle(
+                                color: Colors.greenAccent,
+                                fontSize: 16,
+                                fontFamily: 'Arial',
+                              )),
                           actions: [
                             TextButton(
-                              child: const Text('Yes', style: TextStyle(color: Colors.greenAccent, fontSize: 13, fontWeight: FontWeight.bold, fontFamily: 'Arial',)),
+                              child: const Text('네',
+                                  style: TextStyle(
+                                    color: Colors.greenAccent,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Arial',
+                                  )),
                               onPressed: () => Navigator.of(context).pop(true),
                             ),
                             TextButton(
-                              child: const Text('No', style: TextStyle(color: Colors.greenAccent, fontSize: 13, fontWeight: FontWeight.bold, fontFamily: 'Arial',),),
+                              child: const Text(
+                                '아니요',
+                                style: TextStyle(
+                                  color: Colors.greenAccent,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Arial',
+                                ),
+                              ),
                               onPressed: () => Navigator.of(context).pop(false),
                             ),
                           ],
@@ -116,18 +136,16 @@ class _SettingsState extends State<Settings> {
                         final split = _data.split('/');
 
                         _sessions = {
-                          for (int i = 0; i < split.length; i++)
-                            i: split[i]
+                          for (int i = 0; i < split.length; i++) i: split[i]
                         };
-                        for(int i = 1; i < _sessions.length; i++){
+                        for (int i = 1; i < _sessions.length; i++) {
                           _dates.add(_sessions[i]!.split(' ')[2]);
                         }
-                        for(int i = 1; i < _sessions.length; i++){
+                        for (int i = 1; i < _sessions.length; i++) {
                           _sesh.add(_sessions[i]!.split(' ')[1]);
                         }
                       });
                       _resetTime();
-
                     }
                   },
                 ),
@@ -135,79 +153,79 @@ class _SettingsState extends State<Settings> {
             ),
 
             SingleChildScrollView(
-              padding: const EdgeInsets.all(5.0),
+                padding: const EdgeInsets.all(5.0),
                 physics: ScrollPhysics(),
-
-                child:
-                ScrollConfiguration(
-                    behavior: ScrollConfiguration.of(context).copyWith(dragDevices: {
-                      PointerDeviceKind.touch,
-                      PointerDeviceKind.mouse,
-                    },),
-                child: ListView.builder(
-                    physics: const ScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: _cardNum,
-                    itemBuilder: (BuildContext ctxt, int index) {
-                      return Container(
-                          margin: new EdgeInsets.fromLTRB(20, 0, 20, 5),
-                          width: 5.0,
-                          height: 65.0,
-                          child: Card(
-                            elevation: 15,
-                            shadowColor: Colors.greenAccent,
-                            clipBehavior: Clip.antiAlias,
-                            shape: RoundedRectangleBorder(
-                              side: BorderSide(color: Colors.greenAccent, width: 1),
-                              borderRadius: BorderRadius.circular(25.0),
-                            ),
-                            color: Colors.black,
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                Expanded(
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      children: [
-                                        new Text(
-                                          _dates[_dates.length - 1 - index],
-                                          style: TextStyle(
-                                              color: Colors.greenAccent,
-                                              fontSize: 20,
-                                            fontFamily: 'Arial',),
-                                        ),
-                                        //),
-                                      ],
-                                    ),
-
+                child: ScrollConfiguration(
+                    behavior: ScrollConfiguration.of(context).copyWith(
+                      dragDevices: {
+                        PointerDeviceKind.touch,
+                        PointerDeviceKind.mouse,
+                      },
+                    ),
+                    child: ListView.builder(
+                        physics: const ScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: _cardNum,
+                        itemBuilder: (BuildContext ctxt, int index) {
+                          return Container(
+                              margin: new EdgeInsets.fromLTRB(20, 0, 20, 5),
+                              width: 5.0,
+                              height: 65.0,
+                              child: Card(
+                                elevation: 15,
+                                shadowColor: Colors.greenAccent,
+                                clipBehavior: Clip.antiAlias,
+                                shape: RoundedRectangleBorder(
+                                  side: BorderSide(
+                                      color: Colors.greenAccent, width: 1),
+                                  borderRadius: BorderRadius.circular(25.0),
                                 ),
-                                Expanded(
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      children: [
-                                        new Text(
-                                      _sesh[_sesh.length - 1 - index],
-                                          style: TextStyle(
+                                color: Colors.black,
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          new Text(
+                                            _dates[_dates.length - 1 - index],
+                                            style: TextStyle(
                                               color: Colors.greenAccent,
                                               fontSize: 20,
-                                            fontFamily: 'Arial',),
-                                        ),
-                                        //),
-                                      ],
+                                              fontFamily: 'Arial',
+                                            ),
+                                          ),
+                                          //),
+                                        ],
+                                      ),
                                     ),
-                                  ),
-
-                              ],
-
-                            ),
-
-                          ));
-                    }))),
-
-
-
+                                    Expanded(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          new Text(
+                                            _sesh[_sesh.length - 1 - index],
+                                            style: TextStyle(
+                                              color: Colors.greenAccent,
+                                              fontSize: 20,
+                                              fontFamily: 'Arial',
+                                            ),
+                                          ),
+                                          //),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ));
+                        }))),
 
             // other widgets here
           ],
